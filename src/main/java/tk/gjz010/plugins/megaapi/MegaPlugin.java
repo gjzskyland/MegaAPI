71b069ba0a92c357e8890aa893a1aff2604a5569
gjz010 <gjz010944@gmail.com>
2016-12-08 18:15:14 +0800

    add test interface

--- src/main/java/tk/gjz010/plugins/megaapi/MegaPlugin.java
+++ src/main/java/tk/gjz010/plugins/megaapi/MegaPlugin.java
@@ -1,10 +1,16 @@
 package tk.gjz010.plugins.megaapi;
 
+import com.gmail.stefvanschiedev.customblocks.block.CustomBlock;
 import com.gmail.stefvanschiedev.customblocks.events.listeners.BlockBreakEventListener;
 import java.util.logging.Level;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MegaPlugin extends JavaPlugin {
+public class MegaPlugin extends JavaPlugin implements Listener{
     // This code is called after the server starts and after the /reload command
     @Override
     public void onEnable() {
@@ -16,5 +22,13 @@ public class MegaPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
+    }
+    @EventHandler
+    public void onMessage(AsyncPlayerChatEvent e){
+        if(e.getMessage().contains("create_custom_block")){
+        ItemStack s=new ItemStack(Material.DIAMOND_BLOCK);
+        CustomBlock cb=new CustomBlock(s);
+        cb.place(e.getPlayer().getLocation());
+        }
     }
 }
