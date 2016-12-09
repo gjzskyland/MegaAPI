aba54cc4530323c2b1125b2ded55c7a8c374c00a
gjz010 <gjz010944@gmail.com>
2016-12-09 17:31:57 +0800

    breakable block

--- src/main/java/tk/gjz010/plugins/megaapi/MegaPlugin.java
+++ src/main/java/tk/gjz010/plugins/megaapi/MegaPlugin.java
@@ -26,10 +26,10 @@ public class MegaPlugin extends JavaPlugin implements Listener{
     @Override
     public void onEnable() {
         getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
-        Bukkit.getPluginManager().registerEvents(this, this);
-        Bukkit.getPluginManager().registerEvents(itemManager, this);
         itemManager=new CustomItemManager();
         protocolManager=ProtocolLibrary.getProtocolManager();
+        Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(itemManager, this);
         INSTANCE=this;
     }
 
